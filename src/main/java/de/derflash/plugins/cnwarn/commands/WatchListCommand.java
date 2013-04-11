ebf425696e337f9bb41854d516cdc5debc683537
twonky4 <twonky4@gmx.de>
2013-04-11 17:31:03 +0200

    fixed bug in /watch list

--- src/main/java/de/derflash/plugins/cnwarn/commands/WatchListCommand.java
+++ src/main/java/de/derflash/plugins/cnwarn/commands/WatchListCommand.java
@@ -23,8 +23,18 @@ public class WatchListCommand {
     @CommandPermissions("cubewarn.watch")
     public void listWatch(Player player) {
         List<Watch> watchedPlayers = watchService.getAllWatches();
-        if (watchedPlayers.size() > 0) {
-            chatService.one(player, "staff.watchedPlayer", watchedPlayers);
+
+        StringBuffer playerList = new StringBuffer();
+
+        for (Watch watch : watchedPlayers) {
+            if (playerList.length() > 0) {
+                playerList.append(", ");
+            }
+            playerList.append(watch);
+        }
+
+        if (playerList.length() > 0) {
+            chatService.one(player, "staff.watchedPlayer", playerList.toString());
         } else {
             chatService.one(player, "staff.noWatchPlayers");
         }
