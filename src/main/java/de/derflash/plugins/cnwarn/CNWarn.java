b916bd0ddcc8397bbe6e0d9daaccfb5fd1650f54
twonky4 <twonky4@gmx.de>
2013-12-05 09:09:26 +0100

    simplify for new plugin api

--- src/main/java/de/derflash/plugins/cnwarn/CNWarn.java
+++ src/main/java/de/derflash/plugins/cnwarn/CNWarn.java
@@ -1,22 +1,11 @@
 package de.derflash.plugins.cnwarn;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
-import javax.persistence.PersistenceException;
+import org.bukkit.event.Listener;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import de.cubenation.plugins.utils.chatapi.ChatService;
-import de.cubenation.plugins.utils.commandapi.CommandsManager;
-import de.cubenation.plugins.utils.commandapi.ErrorHandler;
-import de.cubenation.plugins.utils.commandapi.exception.CommandException;
-import de.cubenation.plugins.utils.commandapi.exception.CommandManagerException;
-import de.cubenation.plugins.utils.commandapi.exception.CommandWarmUpException;
-import de.cubenation.plugins.utils.permissionapi.PermissionService;
+import de.cubenation.plugins.utils.pluginapi.BasePlugin;
+import de.cubenation.plugins.utils.pluginapi.CommandSet;
 import de.derflash.plugins.cnwarn.commands.WarnAcceptCommand;
 import de.derflash.plugins.cnwarn.commands.WarnAddCommand;
 import de.derflash.plugins.cnwarn.commands.WarnConfirmCommand;
@@ -33,94 +22,39 @@ import de.derflash.plugins.cnwarn.model.Watch;
 import de.derflash.plugins.cnwarn.services.WarnService;
 import de.derflash.plugins.cnwarn.services.WatchService;
 
-public class CNWarn extends JavaPlugin {
-    // framework services
-    private PermissionService permissionService;
-    private ChatService chatService;
-    private CommandsManager commandsManager;
-
+public class CNWarn extends BasePlugin {
     // local services
     private WarnService warnService;
     private WatchService watchService;
 
     @Override
-    public void onEnable() {
-        setupDatabase();
-
-        permissionService = new PermissionService();
-        chatService = new ChatService(this, permissionService);
-
+    protected void initialCustomServices() {
         warnService = new WarnService(getDatabase(), chatService);
         watchService = new WatchService(getDatabase());
-
-        getServer().getPluginManager().registerEvents(new PlayerListener(warnService, watchService, chatService), this);
-
-        try {
-            commandsManager = new CommandsManager(this);
-            commandsManager.setPermissionInterface(permissionService);
-            commandsManager.setErrorHandler(new ErrorHandler() {
-                @Override
-                public void onError(Exception e) {
-                    getLogger().log(Level.SEVERE, "error on command", e);
-                }
-            });
-            registerCommands();
-        } catch (CommandWarmUpException e) {
-            getLogger().log(Level.SEVERE, "error on register command", e);
-        } catch (CommandManagerException e) {
-            getLogger().log(Level.SEVERE, "error on inital command manager", e);
-        }
-    }
-
-    private void registerCommands() throws CommandWarmUpException {
-        commandsManager.add(WarnAcceptCommand.class, warnService, chatService);
-        commandsManager.add(WarnAddCommand.class, warnService, chatService);
-        commandsManager.add(WarnConfirmCommand.class, warnService);
-        commandsManager.add(WarnDeleteCommand.class, warnService, chatService);
-        commandsManager.add(WarnListCommand.class, warnService, chatService);
-        commandsManager.add(WarnSearchCommand.class, warnService, chatService);
-        commandsManager.add(WatchAddCommand.class, watchService, chatService);
-        commandsManager.add(WatchDeleteCommand.class, watchService, chatService);
-        commandsManager.add(WatchInfoCommand.class, watchService, chatService);
-        commandsManager.add(WatchListCommand.class, watchService, chatService);
     }
 
     @Override
-    public void onDisable() {
-        commandsManager.clear();
-    }
-
-    public void setupDatabase() {
-        try {
-            getDatabase().find(Warn.class).findRowCount();
-            getDatabase().find(Watch.class).findRowCount();
-        } catch (PersistenceException ex) {
-            getLogger().info("Installing database due to first time usage");
-            installDDL();
-        }
+    protected void registerCustomEventListeners(List<Listener> list) {
+        list.add(new PlayerListener(warnService, watchService, chatService));
     }
 
     @Override
-    public List<Class<?>> getDatabaseClasses() {
-        List<Class<?>> list = new ArrayList<Class<?>>();
-        list.add(Watch.class);
+    protected void registerCommands(List<CommandSet> list) {
+        list.add(new CommandSet(WarnAcceptCommand.class, warnService, chatService));
+        list.add(new CommandSet(WarnAddCommand.class, warnService, chatService));
+        list.add(new CommandSet(WarnConfirmCommand.class, warnService));
+        list.add(new CommandSet(WarnDeleteCommand.class, warnService, chatService));
+        list.add(new CommandSet(WarnListCommand.class, warnService, chatService));
+        list.add(new CommandSet(WarnSearchCommand.class, warnService, chatService));
+        list.add(new CommandSet(WatchAddCommand.class, watchService, chatService));
+        list.add(new CommandSet(WatchDeleteCommand.class, watchService, chatService));
+        list.add(new CommandSet(WatchInfoCommand.class, watchService, chatService));
+        list.add(new CommandSet(WatchListCommand.class, watchService, chatService));
+    }
+
+    @Override
+    protected void registerDatabaseModel(List<Class<?>> list) {
         list.add(Warn.class);
-        return list;
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        try {
-            commandsManager.execute(sender, command, label, args);
-        } catch (CommandException e) {
-            getLogger().log(Level.SEVERE, "error on command", e);
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return commandsManager.getTabCompleteList(sender, command, alias, args);
+        list.add(Watch.class);
     }
 }
